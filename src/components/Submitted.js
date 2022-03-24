f80506d3949d535f75fb3b42ebe5dd441569e3d0
Jacks113 <59602950+Jacks113@users.noreply.github.com>
2022-03-24 16:13:15 +0100

    First css adjustment for responsiveness.

--- src/components/Submitted.js
+++ src/components/Submitted.js
@@ -10,7 +10,7 @@ export default function Submitted(props){
 
    return <div className="thanks">
             <div className="submitted-rating">
-                <img src="..\images\illustration-thank-you.svg" />
+                <img src="..\images\illustration-thank-you.svg" alt="thank-you"/>
                 <p className="rated">You selected {rating} out of 5!</p>
                 <h2>Thank you!</h2>
                 <p>We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!</p>
